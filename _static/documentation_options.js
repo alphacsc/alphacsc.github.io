330e6ee58198dd46c115269a68776476180c5c44
alphacsc GitHub Action <alphacsc@github.com>
2022-01-19 20:25:56 +0000

    Update documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g60258ef',
+    VERSION: '0.1.dev1+g7bc49cf',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
